116e8e67d190dc784cd4b972b3c4549bad156712
Janvi Anghan <janvi@vedaha.com>
2026-08-13 16:41:59 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 }
 
-// Health check
+//! Health check
 app.get('/health', (req, res) => {
   res.json({
     success: true,
